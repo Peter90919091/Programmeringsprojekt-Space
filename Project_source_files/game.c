ef421f09d4e9b3455a5c166ae8820b66b4e19faf
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-10 13:17:26 +0100

    Update game.c

--- Project_source_files/game.c
+++ Project_source_files/game.c
@@ -1,3 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <unistd.h>
+#include "system_stm32f30x.h"
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
 void initialize(int time_between) {
 	gotoxy(60,10);
 	printf("Iniatializing game ");
