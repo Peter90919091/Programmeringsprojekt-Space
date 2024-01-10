2d484d76a1ea250f2ce3936f07eda6eaa2e22972
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-10 11:27:24 +0100

    Update Header_file.h

--- Header_files/Header_file.h
+++ Header_files/Header_file.h
@@ -1,1 +1,14 @@
+#ifndef _HEADER_FILE_H_
+#define _HEADER_FILE_H_
+#define ESC 0x1B
+/* Includes -------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdint.h>
 
+/* Exported types -------------------------------------------------------*/
+/* Exported constants ---------------------------------------------------*/
+/* Exported macro -------------------------------------------------------*/
+/* #define			 // Whatever needs to be defined < */
+/* Exported functions -------------------------------------------------- */
+/*void addWhateverFunctionsYouNeed( type parameter );*/
+#endif // _HEADER_FILE_H_
