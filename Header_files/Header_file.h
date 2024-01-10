80fcd2a24b989a2a965883a012c6a9da77233fb0
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-10 17:02:24 +0100

    Update Header_file.h

--- Header_files/Header_file.h
+++ Header_files/Header_file.h
@@ -31,6 +31,7 @@ void write_highscore();
 void write_exitplay();
 void write_play_header();
 void write_numbers(int number,int x,int y)
+void asteroid(int x, int y);
 // #### Controls.c ####
 int ScoreControls();
 int HelpControls();
