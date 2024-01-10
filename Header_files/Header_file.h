d1c4ec63b7cecb879474c04428ff3398a2dab6e7
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-10 11:31:05 +0100

    Update Header_file.h

--- Header_files/Header_file.h
+++ Header_files/Header_file.h
@@ -2,13 +2,38 @@
 #define _HEADER_FILE_H_
 #define ESC 0x1B
 /* Includes -------------------------------------------------------------*/
+#include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <unistd.h>
+#include "system_stm32f30x.h"
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
 
 /* Exported types -------------------------------------------------------*/
 /* Exported constants ---------------------------------------------------*/
 /* Exported macro -------------------------------------------------------*/
 /* #define			 // Whatever needs to be defined < */
 /* Exported functions -------------------------------------------------- */
+// #### MENU_TEXT.c ####
+void write_score();
+void write_play();
+void write_menu();
+void write_help();
+void write_help_header();
+void write_score_header();
+void write_exit();
+void write_Welcome();
+// #### Controls.c ####
+int ScoreControls();
+int HelpControls();
+int MenuControls();
+// #### dataread.c ####
+uint16_t Joystickport();
+// #### ansi.c ####
+
 /*void addWhateverFunctionsYouNeed( type parameter );*/
 #endif // _HEADER_FILE_H_
