4f8e22d2b4964baff175691fecca220c40839db7
EmilNoohr <156067704+EmilNoohr@users.noreply.github.com>
2024-01-10 11:00:47 +0100

    Update ansi.c

--- ansi.c
+++ ansi.c
@@ -1,3 +1,20 @@
 void gotoxy(int x, int y) {
 	printf("%c[%d;%d%c",ESC,y,x,'H');
 }
+void ClearScreen(){
+	printf("%c[2J",ESC);
+	gotoxy(0,0);
+}
+// Move cursor
+void cursor_up(int amount) {
+	printf("%c[%d%c",ESC,amount,'A');
+}
+void cursor_down(int amount) {
+	printf("%c[%d%c",ESC,amount,'B');
+}
+void cursor_right(int amount) {
+	printf("%c[%d%c",ESC,amount,'C');
+}
+void cursor_left(int amount) {
+	printf("%c[%d%c",ESC,amount,'D');
+}
