e8430502353040e32d905745a20242205c822468
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-10 13:14:11 +0100

    Update menu.c

--- Project_source_files/menu.c
+++ Project_source_files/menu.c
@@ -1,3 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <unistd.h>
+#include "system_stm32f30x.h"
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
 void menuHELP() {
 	background();
 	border();
