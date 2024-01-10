66ada970a8e480531c19e3a049de671b916bc940
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-10 11:25:26 +0100

    Create game.c

---
+++ Project_source_files/game.c
@@ -0,0 +1,16 @@
+void initialize(int time_between) {
+	gotoxy(60,10);
+	printf("Iniatializing game ");
+	int third = 0;
+	int second = 0;
+	int first = 0;
+	int remove_text = 0;
+	while (first < time_between) {first += 1;}
+	if (first == time_between) {printf("3");}
+	while (second < time_between) {second += 1;}
+	if (second == time_between) {cursor_left(1);printf("2");}
+	while (third < time_between) {third += 1;}
+	if (third == time_between) {cursor_left(1);printf("1");}
+	while (remove_text < time_between) {remove_text += 1;}
+	if (remove_text == time_between) {cursor_left(20);printf("                    ");}
+}
