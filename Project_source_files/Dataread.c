f9ad76a4b808eb95e3cdb9e08797f332d5465222
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-10 13:14:31 +0100

    Update Dataread.c

--- Project_source_files/Dataread.c
+++ Project_source_files/Dataread.c
@@ -1,3 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <unistd.h>
+#include "system_stm32f30x.h"
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
 uint16_t Joystickport(){
 // PA4 UP
 RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A PA0 = 110
