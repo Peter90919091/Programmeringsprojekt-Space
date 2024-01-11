54029fb30cdf88600ea46baffa4fb389e74c9304
EmilNoohr <156067704+EmilNoohr@users.noreply.github.com>
2024-01-11 17:25:07 +0100

    Update Controls.c

--- Project_source_files/Controls.c
+++ Project_source_files/Controls.c
@@ -14,6 +14,7 @@
 #include "system_stm32f30x.h"
 #include "stm32f30x_conf.h"
 #include "30010_io.h"
+#include "Header_file.h"
 int ScoreControls() {
 	int value = 0;
 	while(1) {value = Joystickport();
