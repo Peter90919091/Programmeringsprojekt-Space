8591f25c6b229afad05771112bb78e4ee93544b3
Peter90919091 <155990182+Peter90919091@users.noreply.github.com>
2024-01-12 15:10:39 +0100

    Update initiate_gameplay.c

--- Project_source_files/initiate_gameplay.c
+++ Project_source_files/initiate_gameplay.c
@@ -24,15 +24,56 @@ void initiate_rocket(int x, int y){
 }
 
 
-void initiate_enemies(int x, int y){
-	void initiate_enemies(int x, int y){
-	fgcolor(1);
-	gotoxy(x,y);
-	printf("%c",219);
-	gotoxy((x-1),y);
-	printf("%c",204);
-	gotoxy((x+1),y);
-	printf("%c",185);
-	fgcolor(15);
+void initiate_enemies(int x, int y) {
+    fgcolor(1);
+    gotoxy(x,y);
+    printf("%c", 204);
+    printf("%c", 219);
+    printf("%c", 185);
+    fgcolor(15);
 }
+
+void clear_enemy(int x, int y) {
+    gotoxy(x, y);
+    printf("   ");
+}
+
+
+void config_timer() {
+    RCC->APB2ENR |= RCC_APB2Periph_TIM15; // Enable clock line to timer 15;
+    TIM15->CR1 |= 0x01; // Enable timer 15
+    TIM15->ARR = 10000; // Set reload value
+    TIM15->PSC = 1000; // Set prescale value
+    TIM15->DIER |= 0x0001; // Enable timer 15 interrupts
+    NVIC_SetPriority(TIM1_BRK_TIM15_IRQn, 0); // Set interrupt priority
+    NVIC_EnableIRQ(TIM1_BRK_TIM15_IRQn); // Enable interrupt
+}
+
+void TIM1_BRK_TIM15_IRQHandler(void) {
+    timer++;
+    TIM15->SR &= ~0x0001; // Clear interrupt bit
+}
+
+
+void enemies_down(int total_enemies[][2], int size) {
+    for (int j = 0; j < size; j++) {
+        clear_enemy(total_enemies[j][0], total_enemies[j][1]);
+        total_enemies[j][1]++;
+        initiate_enemies(total_enemies[j][0], total_enemies[j][1]);
+    }
+}
+void enemies() {
+    static int total_enemies[10][2];
+    static int i = 0;
+    int init_pos_y = 0;
+
+    if (timer == 10) {
+        total_enemies[i][0] = rand()% 71;
+        total_enemies[i][1] = init_pos_y;
+        initiate_enemies(total_enemies[i][0], total_enemies[i][1]);
+
+        timer = 0;
+        i++;
+        enemies_down(total_enemies, i);
+    }
 }
