c1b7431870d34bb2b6c8f466f9e98706613eeffe
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-13 13:14:50 +0100

    Update game.c

--- Project_source_files/game.c
+++ Project_source_files/game.c
@@ -15,6 +15,7 @@
 #include "stm32f30x_conf.h"
 #include "30010_io.h"
 #include "Header_file.h"
+#define MAX_SHOTS 100
 void initialize(int time_between) {
 	gotoxy(60,10);
 	printf("Iniatializing game ");
@@ -30,6 +31,10 @@ void initialize(int time_between) {
 	if (third == time_between) {cursor_left(1);printf("1");}
 	while (remove_text < time_between) {remove_text += 1;}
 	if (remove_text == time_between) {cursor_left(20);printf("                    ");}
+}
+void pause_control(int time_between) {
+	int first = 0;
+	while (first < time_between) {first += 1;}
 }
 void initiate_rocket(int x, int y){
 	y =38;
@@ -67,51 +72,6 @@ void delete_rocket(int x, int y){
 }
 
 
-void initiate_shot(int x, int y) {
-	fgcolor(1);
-	gotoxy(x,y);
-	int SHOTS;
-	for (SHOTS = 0; SHOTS < 33; ) {
-		gotoxy(x,y--);
-		printf("o");
-		cursor_left(1);
-		cursor_down(1);
-		printf(" ");
-		SHOTS += 1;
-
-	}
-	cursor_left(1);
-	printf(" ");
-}
-
-
-void initiate_black_hole(int x,int y){
-	fgcolor(8);
-	gotoxy(x-2,y-1);
-	for (int k=0; k <= 4; k++){
-		printf("%c",187);
-	}
-	cursor_down(1);
-	cursor_left(6);
-	for (int k=0; k <= 6; k++){
-		if (k == 0) {
-			printf("%c",201);
-		}
-		else if (k==6){
-			printf("%c",188);
-		}
-		else{
-			printf("%c",178);
-		}
-	}
-	cursor_down(1);
-	cursor_left(6);
-	for (int k=0; k <=4; k++){
-		printf("%c",200);
-	}
-	fgcolor(15);
-}
-
 void initiate_enemies(int x, int y) {
     fgcolor(1);
     gotoxy(x,y);
@@ -125,6 +85,23 @@ void clear_enemy(int x, int y) {
     gotoxy(x, y);
     printf("   ");
 }
+
+
+void config_timer() {
+    RCC->APB2ENR |= RCC_APB2Periph_TIM15; // Enable clock line to timer 15;
+    TIM15->CR1 |= 0x01; // Enable timer 15
+    TIM15->ARR = 10000; // Set reload value
+    TIM15->PSC = 1000; // Set prescale value
+    TIM15->DIER |= 0x0001; // Enable timer 15 interrupts
+    NVIC_SetPriority(TIM1_BRK_TIM15_IRQn, 0); // Set interrupt priority
+    NVIC_EnableIRQ(TIM1_BRK_TIM15_IRQn); // Enable interrupt
+}
+
+void TIM1_BRK_TIM15_IRQHandler(void) {
+    timer++;
+    TIM15->SR &= ~0x0001; // Clear interrupt bit
+}
+
 
 void enemies_down(int total_enemies[][2], int size) {
     for (int j = 0; j < size; j++) {
@@ -147,6 +124,49 @@ void enemies() {
         i++;
         enemies_down(total_enemies, i);
     }
+}
+
+struct Shot {
+    int x;
+    int y;
+};
+struct Shot shots[MAX_SHOTS];
+int ScoreControls() {
+	int value = 0;
+	while(1) {value = Joystickport();
+	if (value == 16) {menu();}
+	}
+}
+void addShot(int x, int y) {
+    for (int i = 0; i < MAX_SHOTS; ++i) {
+        if (shots[i].x == 0 && shots[i].y == 0) {
+            shots[i].x = x;
+            shots[i].y = y;
+            break;
+        }
+    }
+    for (int i = 0; i < 300000; ++i) {}
+}
+void updateAndPrintShots() {
+    for (int i = 0; i < MAX_SHOTS; ++i) {
+        if (shots[i].x != 0 || shots[i].y != 0) {
+            gotoxy(shots[i].x, shots[i].y);
+
+            if (shots[i].y > 1) {
+                printf("o");
+                cursor_left(1);
+                cursor_down(1);
+                printf(" ");
+                --shots[i].y;
+            } else {
+            	 gotoxy(shots[i].x, ++shots[i].y);
+            	 printf(" ");
+                shots[i].x = 0;
+                shots[i].y = 0;
+            }
+        }
+    }
+    for (int i = 0; i < 60000; ++i) {}
 }
 
 
