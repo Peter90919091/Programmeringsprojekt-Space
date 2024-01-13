e7be5ed6f92a2f0a185a63c04b9d96b50d0937a6
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-13 13:03:06 +0100

    Update levels.c

--- Project_source_files/levels.c
+++ Project_source_files/levels.c
@@ -1,1 +1,61 @@
-
+/*
+ * levels.c
+ *
+ *  Created on: 11. jan. 2024
+ *      Author: Matti
+ */
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <unistd.h>
+#include "system_stm32f30x.h"
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
+void level_1(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
+void level_2(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
+void level_3(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
+void level_4(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
+void level_5(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
+void level_6(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
+void level_7(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
+void level_8(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
+void level_9(){
+	game_border();
+	initialize(3000000);
+	levelControls();
+}
