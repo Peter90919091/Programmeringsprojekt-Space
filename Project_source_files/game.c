c5cb66db1896d1090b0838df95e6f14924bb9647
EmilNoohr <156067704+EmilNoohr@users.noreply.github.com>
2024-01-13 13:27:26 +0100

    Update game.c

--- Project_source_files/game.c
+++ Project_source_files/game.c
@@ -86,23 +86,6 @@ void clear_enemy(int x, int y) {
     printf("   ");
 }
 
-
-void config_timer() {
-    RCC->APB2ENR |= RCC_APB2Periph_TIM15; // Enable clock line to timer 15;
-    TIM15->CR1 |= 0x01; // Enable timer 15
-    TIM15->ARR = 10000; // Set reload value
-    TIM15->PSC = 1000; // Set prescale value
-    TIM15->DIER |= 0x0001; // Enable timer 15 interrupts
-    NVIC_SetPriority(TIM1_BRK_TIM15_IRQn, 0); // Set interrupt priority
-    NVIC_EnableIRQ(TIM1_BRK_TIM15_IRQn); // Enable interrupt
-}
-
-void TIM1_BRK_TIM15_IRQHandler(void) {
-    timer++;
-    TIM15->SR &= ~0x0001; // Clear interrupt bit
-}
-
-
 void enemies_down(int total_enemies[][2], int size) {
     for (int j = 0; j < size; j++) {
         clear_enemy(total_enemies[j][0], total_enemies[j][1]);
