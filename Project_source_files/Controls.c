f079584aa2e01ce72b5f1dd24f671150ccffa860
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-14 15:36:30 +0100

    Update Controls.c

--- Project_source_files/Controls.c
+++ Project_source_files/Controls.c
@@ -15,31 +15,46 @@
 #include "stm32f30x_conf.h"
 #include "30010_io.h"
 #include "Header_file.h"
-int HelpControls() {
-	int value = 0;
-	while(1) {value = Joystickport();
-	if (value == 16) {menu();}
-	}
-}
+volatile uint32_t timer;
+int levelControls(int difficulty) {
+	int speed = 0;
+	int level = 0;
+	int pause = 0;
+	if (difficulty == 1) {speed = 10;level = 1;pause = 3000;}
+	if (difficulty == 2) {speed = 10;level = 2;pause = 2000;}
+	if (difficulty == 3) {speed = 9;level = 3;pause = 1500;}
+	if (difficulty == 4) {speed = 9;level = 4;pause = 1000;}
+	if (difficulty == 5) {speed = 8;level = 5;pause = 1000;}
+	if (difficulty == 6) {speed = 8;level = 6;pause = 1000;}
+	if (difficulty == 7) {speed = 7;level = 7;pause = 1000;}
+	if (difficulty == 8) {speed = 7;level = 8;pause = 1000;}
+	if (difficulty == 9) {speed = 7;level = 9;pause = 1000;}
 
-int levelControls() {
 	int cursor_leftright = 38;
 	int prev_cursor_leftright = 3;
 	int shot_x = 0;
 	int shot_y = 0;
-	int timer = 0;
 	initiate_rocket(cursor_leftright, 0);
-	blink(0);
-	fgcolor(7);
+	text_color_blink(0);
 	int shot = 0;
 	while(1) {
-		enemies();
+
+		gotoxy(0,0);
+		printf("%lu",timer);
+		enemies(speed,level);
 		int value =  Joystickport();
-	if (value == 4 ) { //LEFT
-						if (cursor_leftright> 38) {cursor_leftright -= 2;}
+		if (cursor_leftright == 139 && value == 8) {
+			cursor_leftright = 3;}
+		if (cursor_leftright == 3 && value == 4) {
+					cursor_leftright = 139;}
+
+	if (cursor_leftright > 3 && value == 4 ) { //LEFT
+						if (cursor_leftright> 3) {cursor_leftright -= 1;}
+						pause_control(50000);
 					}
-			if (value == 8) { //RIGHT
-						if (cursor_leftright< 103) {cursor_leftright += 2;}
+			if (cursor_leftright < 139 && value == 8) { //RIGHT
+						if (cursor_leftright< 139) {cursor_leftright += 1;}
+						pause_control(50000);
 					}
 	if (cursor_leftright != prev_cursor_leftright) {
 						blink(0);
@@ -47,7 +62,9 @@ int levelControls() {
 						delete_rocket(prev_cursor_leftright , 0);
 						prev_cursor_leftright = cursor_leftright;
 						initiate_rocket(cursor_leftright, 0);
+						pause_control(100000);
 							}
+
 	if (value == 16) { //SKUD
 		gotoxy(cursor_leftright,35);
 		printf("o");
@@ -55,21 +72,29 @@ int levelControls() {
 		shot_y = 35;
 		shot = 1;
 		addShot(shot_x, shot_y);
+		pause_control(200000);
 		}
 	if (shot == 1) {
-	        updateAndPrintShots();
+	        updateAndPrintShots(1000);
 	    }
 	}
 }
+
+int HelpControls() {
+	int value = 0;
+	while(1) {value = Joystickport();
+	if (value == 16) {menu();}
+	}
+}
+
+
 int PlayControls() {
-		int timer = 0;
 		int cursor_updown = 0;
 		int cursor_leftright = 0;
 		int prev_cursor_updown = 0;
 		int prev_cursor_leftright = 0;
 		int stop = 100;
-		blink(0);
-					fgcolor(7);
+		text_color_blink(0);
 					write_exitplay();
 					write_play_header();
 					write_numbers(1,40,16);
@@ -100,53 +125,43 @@ int PlayControls() {
 		if (value == 16 && cursor_updown == 1 && cursor_leftright== 0 )  { //LEVEL1
 			ClearScreen();
 			level_1();
-			return 1;
 		}
 		if (value == 16 && cursor_updown == 1 && cursor_leftright== 1 )  { //LEVEL2
 					ClearScreen();
 					level_2();
-					return 2;
 				}
 		if (value == 16 && cursor_updown == 1 && cursor_leftright== 2 )  { //LEVEL3
 					ClearScreen();
 					level_3();
-					return 3;
 				}
 		if (value == 16 && cursor_updown == 1 && cursor_leftright== 3 )  { //LEVEL4
 					ClearScreen();
 					level_4();
-					return 4;
 				}
 		if (value == 16 && cursor_updown == 1 && cursor_leftright== 3 )  { //LEVEL5
 							ClearScreen();
 							level_5();
-							return 5;
 						}
 		/// CURSORPUSH level 5-8
 				if (value == 16 && cursor_updown == 2 && cursor_leftright== 0 )  { //LEVEL6
 					ClearScreen();
 					level_6();
-					return 6;
 				}
 				if (value == 16 && cursor_updown == 2 && cursor_leftright== 1 )  { //LEVEL7
 							ClearScreen();
 							level_7();
-							return 7;
 						}
 				if (value == 16 && cursor_updown == 2 && cursor_leftright== 2 )  { //LEVEL8
 							ClearScreen();
 							level_8();
-							return 8;
 						}
 				if (value == 16 && cursor_updown == 2 && cursor_leftright== 3 )  { //LEVEL9
 							ClearScreen();
 							level_9();
-							return 9;
 						}
 		/// CURSOR PLAY
 				if (value == 16 && cursor_updown == 0  )  { //PLAY
 											ClearScreen();
-											return 9;
 										}
 				if (value == 16 && cursor_updown == 3  )  { //EXIT
 															ClearScreen();
@@ -154,8 +169,7 @@ int PlayControls() {
 														}
 
 		if (cursor_leftright != prev_cursor_leftright ||cursor_updown != prev_cursor_updown ) {
-			blink(0);
-			fgcolor(7);
+			text_color_blink(0);
 			write_exitplay();
 			write_play_header();
 			write_numbers(1,40,16);
@@ -171,115 +185,86 @@ int PlayControls() {
 		if (cursor_updown == 0) {
 			if (stop != 0) {
 		prev_cursor_updown = cursor_updown;
-		fgcolor(1);
-		blink(1);
+		text_color_blink(1);
 		write_play_header();
-		fgcolor(7);
-		blink(0);
+		text_color_blink(0);
 		stop = 0;
 			}
 		}
 		if (cursor_updown == 3) {
 			if (stop != 3) {
 			prev_cursor_updown = cursor_updown;
-			fgcolor(1);
-			blink(1);
+			text_color_blink(1);
 			write_exitplay();
-			fgcolor(7);
-			blink(0);
+			text_color_blink(0);
 			stop = 3;}
 				}
 		if (cursor_updown == 1 && cursor_leftright == 0) {
 			if (stop != 10) {
 			prev_cursor_updown = cursor_updown;
 			prev_cursor_leftright = cursor_leftright;
-					fgcolor(2);
-					blink(1);
+			text_color_blink(1);
 					write_numbers(1,40,16);
-					fgcolor(7);
-					blink(0);
+					text_color_blink(0);
 					stop = 10;}
 						}
 		if (cursor_updown == 1 && cursor_leftright == 1) {
 			if (stop != 11) {
 			prev_cursor_updown = cursor_updown;
 			prev_cursor_leftright = cursor_leftright;
-							fgcolor(2);
-							blink(1);
+			text_color_blink(1);
 							write_numbers(2,55,16);
-							fgcolor(7);
-							blink(0);
+							text_color_blink(0);
 							stop = 11;}
 								}
 		if (cursor_updown == 1 && cursor_leftright == 2 ) {
 			if (stop != 12) {
-			prev_cursor_updown = cursor_updown;
-			prev_cursor_leftright = cursor_leftright;
-		fgcolor(2);
-		blink(1);
+		prev_cursor_updown = cursor_updown;
+		prev_cursor_leftright = cursor_leftright;
+		text_color_blink(1);
 		write_numbers(3,70,16);
-		fgcolor(7);
-		blink(0);
-		stop = 12;}
-		}
+		text_color_blink(0);
+		stop = 12;}	}
 		if (cursor_updown == 1 && cursor_leftright == 3 ) {
 			if (stop != 13) {
-			prev_cursor_updown = cursor_updown;
-						prev_cursor_leftright = cursor_leftright;
-		fgcolor(2);
-		blink(1);
+		prev_cursor_updown = cursor_updown;
+		prev_cursor_leftright = cursor_leftright;
+		text_color_blink(1);
 		write_numbers(4,85,16);
-		fgcolor(7);
-		blink(0);
-		stop = 13;}
-
-		}
+		text_color_blink(0);
+		stop = 13;}	}
 		if (cursor_updown == 1 && cursor_leftright == 4 ) {
 			if (stop != 14) {
-			prev_cursor_updown = cursor_updown;
-						prev_cursor_leftright = cursor_leftright;
-		fgcolor(2);
-		blink(1);
+		prev_cursor_updown = cursor_updown;
+		prev_cursor_leftright = cursor_leftright;
+		text_color_blink(1);
 		write_numbers(5,100,16);
-		fgcolor(7);
-		blink(0);
-		stop = 14;}
-
-		}
+		text_color_blink(0);
+		stop = 14;}	}
 		if (cursor_updown == 2 && cursor_leftright == 0 ) {
 			if (stop != 20) {
-			prev_cursor_updown = cursor_updown;
-						prev_cursor_leftright = cursor_leftright;
-		fgcolor(2);
-		blink(1);
+		prev_cursor_updown = cursor_updown;
+		prev_cursor_leftright = cursor_leftright;
+		text_color_blink(1);
 		write_numbers(6,40,24);
-		fgcolor(7);
-		blink(0);
-		stop = 20;}
-
-		}
+		text_color_blink(0);
+		stop = 20;}	}
 		if (cursor_updown == 2 && cursor_leftright == 1 ) {
 			if (stop != 21) {
 			prev_cursor_updown = cursor_updown;
-						prev_cursor_leftright = cursor_leftright;
-		fgcolor(2);
-		blink(1);
-		write_numbers(7,55,24);
-		fgcolor(7);
-		blink(0);
-		stop = 21;}
-		}
+			prev_cursor_leftright = cursor_leftright;
+			text_color_blink(1);
+			write_numbers(7,55,24);
+			text_color_blink(0);
+			stop = 21;}	}
 		if (cursor_updown == 2 && cursor_leftright == 2 ) {
 			if (stop != 22) {
 		prev_cursor_updown = cursor_updown;
 		prev_cursor_leftright = cursor_leftright;
-		fgcolor(2);
-		blink(1);
+		text_color_blink(1);
 		write_numbers(8,70,24);
-		fgcolor(7);
-		blink(0);
-		stop = 22;}
-		}
+		text_color_blink(0);
+		stop = 22;}	}
 		if (cursor_updown == 2 && cursor_leftright == 3 ) {
 			if (stop != 23) {
 		prev_cursor_updown = cursor_updown;
@@ -287,16 +272,19 @@ int PlayControls() {
 		text_color_blink(1);
 		write_numbers(9,85,24);
 		text_color_blink(0);
-		stop = 23;}
-		}
+		stop = 23;}	}
 		if (value != 0) {
 			pause_control(1000000);}}
 }
-
+int ScoreControls() {
+	int value = 0;
+	while(1) {value = Joystickport();
+	if (value == 16) {menu();}
+	}
+}
 
 
 int MenuControls() {
-	int timer = 0;
 	int cursor_placement = 0;
 	int prev_cursor_placement = 0;
 	int stop = 100;
