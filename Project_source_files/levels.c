8ad05b6e20ce85303b8e86964b8366698f5e246d
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-14 15:38:50 +0100

    Update levels.c

--- Project_source_files/levels.c
+++ Project_source_files/levels.c
@@ -14,48 +14,70 @@
 #include "system_stm32f30x.h"
 #include "stm32f30x_conf.h"
 #include "30010_io.h"
+#include "Header_file.h"
 void level_1(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+	timer = 0;
+	while(1) {
+	levelControls(1);
+	}
 }
 void level_2(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+		timer = 0;
+	while(1) {
+		levelControls(2);
+		}
 }
 void level_3(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+		timer = 0;
+	while(1) {
+		levelControls(3);
+		}
 }
 void level_4(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+		timer = 0;
+	while(1) {
+		levelControls(4);
+		}
 }
 void level_5(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+		timer = 0;
+	while(1) {
+		levelControls(5);
+		}
 }
 void level_6(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+		timer = 0;
+	while(1) {
+		levelControls(6);
+		}
 }
 void level_7(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+		timer = 0;
+	while(1) {
+		levelControls(7);
+		}
 }
 void level_8(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+		timer = 0;
+	while(1) {
+		levelControls(8);
+		}
 }
 void level_9(){
-	game_border();
-	initialize(3000000);
-	levelControls();
+	border();
+		timer = 0;
+	while(1) {
+		levelControls(9);
+		}
+}
+void play_whole_game() {
+
 }
