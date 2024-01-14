9b816f28c02f202c2646736a530f1653cc954845
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-14 15:45:57 +0100

    Update Header_file.h

--- Header_files/Header_file.h
+++ Header_files/Header_file.h
@@ -1,6 +1,7 @@
 #ifndef _HEADER_FILE_H_
 #define _HEADER_FILE_H_
 #define ESC 0x1B
+
 /* Includes -------------------------------------------------------------*/
 #include <string.h>
 #include <stdio.h>
@@ -12,6 +13,7 @@
 #include "system_stm32f30x.h"
 #include "stm32f30x_conf.h"
 #include "30010_io.h"
+volatile uint32_t timer;
 
 /* Exported types -------------------------------------------------------*/
 /* Exported constants ---------------------------------------------------*/
@@ -38,19 +40,20 @@ int ScoreControls();
 int HelpControls();
 int MenuControls();
 int PlayControls();
-int levelControls();
+int levelControls(int difficulty);
 
 // #### dataread.c ####
 uint16_t Joystickport();
 void lcd_resetwrite(const char* input_string, int line_number);
-void lcd_write_index(const char* input_string,int line_number, int index);
-void lcd_write_array(uint8_t array[],int line_number,int index,int size);
+void lcd_write_index(const char* input_string, int line_number, int index);
+void lcd_write_array(uint8_t array[], int line_number, int index, int size);
 void write_heart(int line_number, int index);
 void clear_line(int line_number);
 
+
 // #### ansi.c ####
 void fgcolor(uint8_t foreground);
-void blink(uint8_t on);
+void blink();
 void voidgotoxy();
 void ClearScreen();
 void cursor_up(int amount);
@@ -60,6 +63,7 @@ void cursor_left(int amount);
 
 // #### design.c ####
 void border();
+void text_color_blink(int on);
 void background();
 void menubox();
 void box();
@@ -68,7 +72,10 @@ void credits();
 void Smallbox();
 void game_border();
 void help_design();
-void text_color_blink(int on);
+void displayAsteroidBySize(char size, struct Asteroid asteroid);
+void displaySmallAsteroid(struct Asteroid asteroid);
+void displayMediumAsteroid(struct Asteroid asteroid);
+void displayBigAsteroid(struct Asteroid asteroid);
 
 // #### menu.c ####
 void menuHELP();
@@ -76,25 +83,26 @@ void menuSCORE();
 void menuPLAY();
 void menu();
 
+//initiate_gameplay.c
+void initiate_rocket(int x1, int y1);
+
 // #### game.c ####
 void initiate_rocket(int x, int y);
-void initiate_enemies(int x, int y);
+void initiate_enemies(int x, int y,int color);
 void clear_enemy(int x, int y);
 void config_timer();
-void enemies_down(int total_enemies[][2], int size);
-void enemies();
+void enemies(int speed,int level);
 void initialize(int time_between);
 void delete_rocket(int x, int y);
 void initiate_shot(int x, int y);
 void initialize_screen();
 void display_screen();
-void initiate_shot(int x, int y);
-void initiate_black_hole(int x,int y);
-void TIM1_BRK_TIM15_IRQHandler(void);
-int ScoreControls();
+void pause_control(int time_between);
+void points(int points);
 void addShot(int x, int y);
-void updateAndPrintShots();
-
+bool isCollision(int shotX, int shotY, int enemyX, int enemyY);
+void updateAndPrintShots(int pause);
+void initiate_black_hole(int x,int y);
 
 //#### levels.c ####
 void level_1();
