e46075f81a9779819971cf98c7898f9bb2ae886a
EmilNoohr <156067704+EmilNoohr@users.noreply.github.com>
2024-01-15 11:55:24 +0100

    Update main.c

--- main_file/main.c
+++ main_file/main.c
@@ -1,14 +1,15 @@
 #include "Header_file.h"
 int main(void) {
-	uart_init(9600);
+	uart_init(115200);
 	lcd_init();
+	config_timer();
 	hideCursor();
 	blink(0);
 	ClearScreen();
-	menu();
 
 	//MenuControls();
 	while (1) {
+	menu();
 	}
 	return 0;
 }
