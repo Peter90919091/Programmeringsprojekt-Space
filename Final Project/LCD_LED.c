f045d23ecda880dfc10af1ec41c19e0050f442ab
EmilNoohr <156067704+EmilNoohr@users.noreply.github.com>
2024-01-18 11:57:16 +0100

    Update LCD_LED.c

--- Final Project/LCD_LED.c
+++ Final Project/LCD_LED.c
@@ -300,58 +300,199 @@ void level_lcd(int level, int globalLives, int globalPoints) {
 
 	lcd_push_buffer(array);
 }
+//Void function used to turn on/off the LED on the microcontroller with an integer as input
+//
+void LED(int colour){
+	/*Colour table for LED on microcontroller
+	| Red     1
+	| Green   2
+	| Blue    3
+	| Cyan    4
+	| Yellow  5
+	| Purple  6
+	| White   7
+	| Off     8
+	*/
 
-void setLed(int value) {
-	if (value == 1) { // blue PA9
-				GPIOA->ODR &= ~(1 << 9); //Set pin PA1 to high
-				GPIOA-> BSRR &= ~(1<<9);
-				RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A PA0 = 110
-				GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2)); // Clear speed register
-				GPIOA->OSPEEDR |= (0x00000002 << (9 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
-				GPIOA->OTYPER &= ~(0x0001 << (9 * 1)); // Clear output type register
-				GPIOA->OTYPER |= (0x0000 << (9)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
-				GPIOA->MODER &= ~(0x00000003 << (9 * 2)); // Clear mode register
-				GPIOA->MODER |= (0x00000001 << (9 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
-				GPIOA->ODR |= (0 << 9); //Set pin PA1 to high
-				GPIOA-> BSRR |= (0<<9);
-			}
-	if (value == 2) { // grøn PC7
-		GPIOC->ODR &= ~(1 << 7); //Set pin PA1 to high
-		GPIOC-> BSRR &= ~(1<<7);
-			RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port A PA0 = 110
-			GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2)); // Clear speed register
-			GPIOC->OSPEEDR |= (0x00000002 << (7 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
-			GPIOC->OTYPER &= ~(0x0001 << (7 * 1)); // Clear output type register
-			GPIOC->OTYPER |= (0x0000 << (7)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
-			GPIOC->MODER &= ~(0x00000003 << (7 * 2)); // Clear mode register
-			GPIOC->MODER |= (0x00000001 << (7 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
-			GPIOC->ODR |= (0 << 7); //Set pin PA1 to high
-			GPIOC-> BSRR |= (0<<7);
-		}
-	if (value == 3) { // red PB4
-				GPIOB->ODR &= ~(1 << 4); //Set pin PA1 to high
-				GPIOB-> BSRR &= ~(1<<4);
-				RCC->AHBENR |= RCC_AHBPeriph_GPIOB; // Enable clock for GPIO Port A PA0 = 110
-				GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2)); // Clear speed register
-				GPIOB->OSPEEDR |= (0x00000002 << (4 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
-				GPIOB->OTYPER &= ~(0x0001 << (4 * 1)); // Clear output type register
-				GPIOB->OTYPER |= (0x0000 << (4)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
-				GPIOB->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
-				GPIOB->MODER |= (0x00000001 << (4 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
-				GPIOB->ODR |= (0 << 4); //Set pin PA1 to high
-				GPIOB-> BSRR |= (0<<4);
-			}
-	if (value == 0) {
-		RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A PA0 = 110
-				GPIOA->ODR |= (1 << 9); //Set pin PA1 to high
-				GPIOA-> BSRR |= (1<<9);
-				RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
-				GPIOC->ODR |= (1 << 7); //Set pin PA1 to high
-				GPIOC-> BSRR |= (1<<7);
+	switch (colour) {
+	case 1: //Red
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+		GPIOB->ODR &= ~(1<<4);
+		GPIOB->BSRR &= ~(1<<4);
+		GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2)); // Clear speed register
+		GPIOB->OSPEEDR |= (0x00000002 << (4 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOB->OTYPER &= ~(0x0001 << (4 * 1)); // Clear output type register
+		GPIOB->OTYPER |= (0x0000 << (4)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOB->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
+		GPIOB->MODER |= (0x00000001 << (4 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOB->ODR |= (0 << 4);
+		GPIOB->BSRR |= (0 << 4);
+		break;
+	case 2: //Green
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+		GPIOC->ODR &= ~(1<<7);
+		GPIOC->BSRR &= ~(1<<7);
+		GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2)); // Clear speed register
+		GPIOC->OSPEEDR |= (0x00000002 << (7 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOC->OTYPER &= ~(0x0001 << (7 * 1)); // Clear output type register
+		GPIOC->OTYPER |= (0x0000 << (7)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOC->MODER &= ~(0x00000003 << (7 * 2)); // Clear mode register
+		GPIOC->MODER |= (0x00000001 << (7 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOC->ODR |= (0 << 7);
+		GPIOC->BSRR |= (0 << 7);
+		break;
+	case 3: //Blue
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOA;
+		GPIOA->ODR &= ~(1<<9);
+		GPIOA->BSRR &= ~(1<<9);
+		GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2)); // Clear speed register
+		GPIOA->OSPEEDR |= (0x00000002 << (9 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOA->OTYPER &= ~(0x0001 << (9 * 1)); // Clear output type register
+		GPIOA->OTYPER |= (0x0000 << (9)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOA->MODER &= ~(0x00000003 << (9 * 2)); // Clear mode register
+		GPIOA->MODER |= (0x00000001 << (9 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOA->ODR |= (0 << 9);
+		GPIOA->BSRR |= (0 << 9);
+		break;
+	case 4: //Cyan
+		//Activating Blue
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOA;
+		GPIOA->ODR &= ~(1<<9);
+		GPIOA->BSRR &= ~(1<<9);
+		GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2)); // Clear speed register
+		GPIOA->OSPEEDR |= (0x00000002 << (9 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOA->OTYPER &= ~(0x0001 << (9 * 1)); // Clear output type register
+		GPIOA->OTYPER |= (0x0000 << (9)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOA->MODER &= ~(0x00000003 << (9 * 2)); // Clear mode register
+		GPIOA->MODER |= (0x00000001 << (9 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOA->ODR |= (0 << 9);
+		GPIOA->BSRR |= (0 << 9);
 
-				RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
-				GPIOB->ODR |= (1 << 4); //Set pin PA1 to high
-				GPIOB-> BSRR |= (1<<4);
+		//Activating Green
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+		GPIOC->ODR &= ~(1<<7);
+		GPIOC->BSRR &= ~(1<<7);
+		GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2)); // Clear speed register
+		GPIOC->OSPEEDR |= (0x00000002 << (7 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOC->OTYPER &= ~(0x0001 << (7 * 1)); // Clear output type register
+		GPIOC->OTYPER |= (0x0000 << (7)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOC->MODER &= ~(0x00000003 << (7 * 2)); // Clear mode register
+		GPIOC->MODER |= (0x00000001 << (7 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOC->ODR |= (0 << 7);
+		GPIOC->BSRR |= (0 << 7);
+		break;
+	case 5: //Yellow
+		//Activating Green
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+		GPIOC->ODR &= ~(1<<7);
+		GPIOC->BSRR &= ~(1<<7);
+		GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2)); // Clear speed register
+		GPIOC->OSPEEDR |= (0x00000002 << (7 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOC->OTYPER &= ~(0x0001 << (7 * 1)); // Clear output type register
+		GPIOC->OTYPER |= (0x0000 << (7)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOC->MODER &= ~(0x00000003 << (7 * 2)); // Clear mode register
+		GPIOC->MODER |= (0x00000001 << (7 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOC->ODR |= (0 << 7);
+		GPIOC->BSRR |= (0 << 7);
+
+		//Activating Red
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+		GPIOB->ODR &= ~(1<<4);
+		GPIOB->BSRR &= ~(1<<4);
+		GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2)); // Clear speed register
+		GPIOB->OSPEEDR |= (0x00000002 << (4 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOB->OTYPER &= ~(0x0001 << (4 * 1)); // Clear output type register
+		GPIOB->OTYPER |= (0x0000 << (4)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOB->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
+		GPIOB->MODER |= (0x00000001 << (4 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOB->ODR |= (0 << 4);
+		GPIOB->BSRR |= (0 << 4);
+		break;
+	case 6: //Purple
+		//Activating Red
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+		GPIOB->ODR &= ~(1<<4);
+		GPIOB->BSRR &= ~(1<<4);
+		GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2)); // Clear speed register
+		GPIOB->OSPEEDR |= (0x00000002 << (4 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOB->OTYPER &= ~(0x0001 << (4 * 1)); // Clear output type register
+		GPIOB->OTYPER |= (0x0000 << (4)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOB->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
+		GPIOB->MODER |= (0x00000001 << (4 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOB->ODR |= (0 << 4);
+		GPIOB->BSRR |= (0 << 4);
+
+		//Activating Blue
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOA;
+		GPIOA->ODR &= ~(1<<9);
+		GPIOA->BSRR &= ~(1<<9);
+		GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2)); // Clear speed register
+		GPIOA->OSPEEDR |= (0x00000002 << (9 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOA->OTYPER &= ~(0x0001 << (9 * 1)); // Clear output type register
+		GPIOA->OTYPER |= (0x0000 << (9)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOA->MODER &= ~(0x00000003 << (9 * 2)); // Clear mode register
+		GPIOA->MODER |= (0x00000001 << (9 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOA->ODR |= (0 << 9);
+		GPIOA->BSRR |= (0 << 9);
+		break;
+
+	case 7: //White
+		//Activating Blue
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOA;
+		GPIOA->ODR &= ~(1<<9);
+		GPIOA->BSRR &= ~(1<<9);
+		GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2)); // Clear speed register
+		GPIOA->OSPEEDR |= (0x00000002 << (9 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOA->OTYPER &= ~(0x0001 << (9 * 1)); // Clear output type register
+		GPIOA->OTYPER |= (0x0000 << (9)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOA->MODER &= ~(0x00000003 << (9 * 2)); // Clear mode register
+		GPIOA->MODER |= (0x00000001 << (9 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOA->ODR |= (0 << 9);
+		GPIOA->BSRR |= (0 << 9);
+
+		//Activating Green
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+		GPIOC->ODR &= ~(1<<7);
+		GPIOC->BSRR &= ~(1<<7);
+		GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2)); // Clear speed register
+		GPIOC->OSPEEDR |= (0x00000002 << (7 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOC->OTYPER &= ~(0x0001 << (7 * 1)); // Clear output type register
+		GPIOC->OTYPER |= (0x0000 << (7)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOC->MODER &= ~(0x00000003 << (7 * 2)); // Clear mode register
+		GPIOC->MODER |= (0x00000001 << (7 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOC->ODR |= (0 << 7);
+		GPIOC->BSRR |= (0 << 7);
+
+		//Activating Red
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+		GPIOB->ODR &= ~(1<<4);
+		GPIOB->BSRR &= ~(1<<4);
+		GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2)); // Clear speed register
+		GPIOB->OSPEEDR |= (0x00000002 << (4 * 2)); // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
+		GPIOB->OTYPER &= ~(0x0001 << (4 * 1)); // Clear output type register
+		GPIOB->OTYPER |= (0x0000 << (4)); // Set output type register (0x00 -Push pull, 0x01 - Open drain)
+		GPIOB->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
+		GPIOB->MODER |= (0x00000001 << (4 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+		GPIOB->ODR |= (0 << 4);
+		GPIOB->BSRR |= (0 << 4);
+		break;
+
+	case 8:
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOA;
+		GPIOA->ODR |= (1<<9);
+		GPIOA-> BSRR |= (1<<9);
+
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+		GPIOB->ODR |= (1<<4);
+		GPIOB->BSRR |= (1<<4);
+
+		RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+		GPIOC->ODR |= (1<<7);
+		GPIOC->BSRR |= (1<<7);
+		break;
+	default:
+		break;
 	}
 
 }
+
