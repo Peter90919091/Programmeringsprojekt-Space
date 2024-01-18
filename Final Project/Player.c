7b9d853dcba8048ad3ca8f4ef2fa670972e2ad31
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-18 11:07:44 +0100

    Update Player.c

--- Final Project/Player.c
+++ Final Project/Player.c
@@ -1,3 +1,20 @@
+/*
+ * Menu_text.c
+ *
+ *  Created on: 10. jan. 2024
+ *      Author: Matti
+ */
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <unistd.h>
+#include "system_stm32f30x.h"
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
+#include "Header_file.h"
 void initiate_rocket(int x, int y){
 	gotoxy(x,(y-1));
 	printf("%c",186);
