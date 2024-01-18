6a411806b5f3ba2df29ebe7fff71593e53e97fb8
MattiasDTU <155991703+MattiasDTU@users.noreply.github.com>
2024-01-18 11:07:23 +0100

    Update Player.c

--- Final Project/Player.c
+++ Final Project/Player.c
@@ -1,1 +1,27 @@
+void initiate_rocket(int x, int y){
+	gotoxy(x,(y-1));
+	printf("%c",186);
+	gotoxy(x,y);
+	printf("%c",219);
 
+	fgcolor(1);
+	gotoxy(x,(y+1));
+	printf("%c",177);
+	fgcolor(15);
+
+	gotoxy((x-1),(y+1));
+	printf("%c",47);
+	gotoxy((x+1),(y+1));
+	printf("%c",92);
+}
+void delete_rocket(int x, int y){
+	gotoxy(x,(y-1));printf(" ");
+	gotoxy(x,y);printf(" ");
+
+	fgcolor(1);
+	gotoxy(x,(y+1));printf(" ");
+	fgcolor(15);
+
+	gotoxy((x-1),(y+1));printf(" ");
+	gotoxy((x+1),(y+1));printf(" ");
+}
