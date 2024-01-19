e9a0cacbfca732c8db062944796d32946eb5773c
IdaDTU <156414206+IdaDTU@users.noreply.github.com>
2024-01-19 12:46:51 +0100

    Update Player.c with comments

--- Final Project/Player.c
+++ Final Project/Player.c
@@ -9,23 +9,27 @@
 #include "stm32f30x_conf.h"
 #include "30010_io.h"
 #include "Header_file.h"
+// Funktion til at initialisere en raket på skærmen ved de angivne koordinater (x, y).
 void initiate_rocket(int x, int y){
+	 // Placerer raketten på skærmen med forskellige tegn og farver
 	gotoxy(x,(y-1));
 	printf("%c",186);
 	gotoxy(x,y);
 	printf("%c",219);
 
-	fgcolor(1);
+	fgcolor(1); // Skifter farven til rød
 	gotoxy(x,(y+1));
 	printf("%c",177);
-	fgcolor(15);
+	fgcolor(15); // Skifter farven tilbage til hvid
 
 	gotoxy((x-1),(y+1));
 	printf("%c",47);
 	gotoxy((x+1),(y+1));
 	printf("%c",92);
 }
+// Funktion til at slette raketten fra skærmen ved de angivne koordinater (x, y)
 void delete_rocket(int x, int y){
+	// Sletter raketten fra skærmen ved at overstrege tegn med mellemrum.
 	gotoxy(x,(y-1));printf(" ");
 	gotoxy(x,y);printf(" ");
 
