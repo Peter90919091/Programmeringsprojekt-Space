4f1950ecc474572a7bdbd4a1e79b0996c61619eb
IdaDTU <156414206+IdaDTU@users.noreply.github.com>
2024-01-19 13:49:49 +0100

    Update main.c with comments

--- Final Project/main.c
+++ Final Project/main.c
@@ -1,16 +1,16 @@
 #include "Header_file.h"
 int main(void) {
-	uart_init(115200);
-	lcd_init();
-	config_timer();
-	inittim2();
-	hideCursor();
-	blink(0);
-	ClearScreen();
-	setFreq(0);
+	uart_init(115200); // Initialiserer UART-kommunikation med en baudrate på 115200
+	lcd_init(); // Initialiserer LCD-skærmen
+	config_timer(); // Konfigurerer timeren
+	inittim2(); // Initialiserer timer 2
+	hideCursor(); // Skjuler kursor på skærmen
+	blink(0); // Aktiverer blink-effekten på skærmen.
+	ClearScreen(); // Rydder skærmen
+	setFreq(0); // Indstiller frekvensen til 0
 	
 	while (1) {
-		ChooseUserInput();
+		ChooseUserInput(); // Udfører et loop, hvor brugeren kan vælge input.
 	}
-	return 0;
+	return 0; // Afslutter programmet.
 }
